1e8b5cbb9f867a99137da7cca09fbaf1c251aa23
ZxyKira <danny41553@gmail.com>
2021-04-20 17:20:26 +0800

    [remove] flag status

--- include/tool_fifo.h
+++ include/tool_fifo.h
@@ -4,157 +4,56 @@
  *      Modufy Date  :2021-03-28
  *      Information :
  */
-#ifndef tool_fifo_H
-#define tool_fifo_H
+#ifndef tool_fifo_H_
+#define tool_fifo_H_
 
 #include <stdint.h>
 #include <stdbool.h> 
 
 #ifdef __cplusplus
 extern "C" {
-#endif
+#endif //__cplusplus
 
-#define fifo_status_inited         (1 << 0)
-#define fifo_statis_isFull         (1 << 1)
-#define fifo_status_isOverride     (1 << 2)
-#define fifo_status_isMutexHead    (1 << 4)
-#define fifo_status_isMutexTail    (1 << 5)
-
-#define fifo_func_logRecode     (1 << 0)
-#define fifo_func_override      (1 << 1)
-#define fifo_func_mutex         (1 << 2)
-
+/**
+ * @def    RB_VHEAD(rb)
+ * volatile typecasted head index
+ */
+#define RB_VHEAD(rb)              (*(volatile uint32_t *) &(rb)->head)
+  
+/**
+ * @def    RB_VTAIL(rb)
+ * volatile typecasted tail index
+ */
+#define RB_VTAIL(rb)              (*(volatile uint32_t *) &(rb)->tail)
 
 /* *****************************************************************************************
- *    Struct - tool_fifo_t
+ *    Struct - tool_fifo_t* _this_t
  */ 
-typedef struct {
+typedef struct _tool_fifo_t{
   void *data;
   uint32_t count;
   uint32_t itemSz;
   uint32_t head;
   uint32_t tail;
-  const char *name;
-  struct{
-    uint8_t status;
-    uint8_t func;
-    uint8_t reserved[2];
-  }config;
 }tool_fifo_t;
 
 /* *****************************************************************************************
- *    Struct - tool_fifo_config_t
- */ 
-typedef struct{
-  void* address;
-  void* buffer;
-  uint32_t bufferSize;
-  int count;
-  int itemSize;
-}tool_fifo_config_t;
-
-/* *****************************************************************************************
- *    Struct - tool_fifo_api_t
+ *    Struct - tool_fifo_t* _this_api_t
  */ 
 typedef struct _tool_fifo_api_t{
-  void (*flush)(tool_fifo_t *fifo);
-  /**
-   * @brief  Clear FIFO queue.
-   * @param  fifo  : FIFO Handler memory pointer.
-   * @return  None
-   */
-  uint32_t (*getSize)(tool_fifo_t *fifo);
-  /**
-   * @brief  Get FIFO block size.
-   * @param  fifo  : FIFO Handler memory pointer.
-   * @return  Size of single block .
-   */
-  uint32_t (*getCount)(tool_fifo_t *fifo);
-  /**
-   * @brief  Get FIFO usable count.
-   * @param  fifo  : FIFO Handler memory pointer.
-   * @return  Totla of usable block count.
-   */
-  uint32_t (*getFree)(tool_fifo_t *fifo);
-  /**
-   * @brief  Get FIFO unused block count
-   * @param  fifo  : FIFO Handler memory pointer.
-   * @return  Totla of unused block count.
-   */
-  uint32_t (*getUsed)(tool_fifo_t *fifo);
-  /**
-   * @brief  Get FIFO used block count
-   * @param  fifo  : FIFO Handler memory pointer.
-   * @return  Totla of used block count.
-   */
-  uint8_t (*getStatus)(tool_fifo_t *fifo);
-  bool (*isFull)(tool_fifo_t *fifo);
-  /**
-   * @brief  Get FIFO status.
-   * @param  fifo  : FIFO Handler memory pointer.
-   * @return  0:not full; 1:is full.
-   */
-  bool (*isEmpty)(tool_fifo_t *fifo);
-  /**
-   * @brief  Get FIFO status.
-   * @param  fifo  : FIFO Handler memory pointer.
-   * @return  0:not empty; 1:is empty.
-   */
-  bool (*init)(tool_fifo_t *fifo,  const tool_fifo_config_t *config);
-  /**
-   * @brief  Init FIFO<Normal mod>.
-   * @param  fifo  : FIFO Handler memory pointer.
-   * @param  config: FIFO config pointer.
-   * @return  0:Init fail; 1:Init successful.
-   */  
-  tool_fifo_t* (*initAddr)(const tool_fifo_config_t *config);
-  /**
-   * @brief  Init FIFO<Address mod>.
-   * @param  config: FIFO config pointer.
-   * @return  0:Init fail; 1:Init successful.
-   */  
-  tool_fifo_t* (*reload)(const tool_fifo_config_t *config);
-  /**
-   * @brief  reload FIFO<Address mod>.
-   * @param  config: FIFO config pointer.
-   * @return  0:Init fail; 1:Init successful.
-   */  
-  bool (*insert)(tool_fifo_t *fifo, const void *data);
-  /**
-   * @brief  Add new data to FIFO.
-   * @param  fifo  : FIFO Handler memory pointer.
-   * @param  data  : Data pointer.
-   * @return  0:FIFO is full ; 1:Successful.
-   */  
-   
-  int (*insertMulti)(tool_fifo_t *fifo, const void *data, uint32_t count);
-  
-  bool (*insertTail)(tool_fifo_t *fifo, const void *data);
-  /**
-   * @brief  Add new data to FIFO tail<priority high>.
-   * @param  fifo  : FIFO Handler memory pointer.
-   * @param  data  : Data pointer.
-   * @return  0:FIFO is full ; 1:Successful.
-   */  
-  bool (*pop)(tool_fifo_t *fifo, void *buffer);
-  /**
-   * @brief  pop data form FIFO.
-   * @param  fifo  : FIFO Handler memory pointer.
-   * @param  data  : Data out buffer pointer.
-   * @return  0:FIFO is empty ; 1:Successful.
-   */  
-   
-  int (*popMulti)(tool_fifo_t *fifo, void *buffer, uint32_t count);
-  
-  bool (*popHead)(tool_fifo_t *fifo, void *buffer);
-  /**
-   * @brief  pop data form FIFO Head.
-   * @param  fifo  : FIFO Handler memory pointer.
-   * @param  data  : Data out buffer pointer.
-   * @return  0:FIFO is empty ; 1:Successful.
-   */  
-  void (*unsetFunc)(tool_fifo_t *fifo, uint8_t setting);
-  void (*setFunc)(tool_fifo_t *fifo, uint8_t setting);
+  void     (*flush)       (tool_fifo_t* _this);
+  uint32_t (*getSize)     (tool_fifo_t* _this);
+  uint32_t (*getCount)    (tool_fifo_t* _this);
+  uint32_t (*getFree)     (tool_fifo_t* _this);
+  bool     (*isFull)      (tool_fifo_t* _this);
+  bool     (*isEmpty)     (tool_fifo_t* _this);
+  bool     (*init)        (tool_fifo_t* _this, void *buffer, int itemSize, int count);
+  bool     (*insert)      (tool_fifo_t* _this, const void *data);
+  int      (*insertMulti) (tool_fifo_t* _this, const void *data, uint32_t count);
+  bool     (*insertTail)  (tool_fifo_t* _this, const void *data);
+  bool     (*pop)         (tool_fifo_t* _this, void *buffer);
+  int      (*popMulti)    (tool_fifo_t* _this, void *buffer, uint32_t count);
+  bool     (*popHead)     (tool_fifo_t* _this, void *buffer);
 }tool_fifo_api_t;
 
 /* *****************************************************************************************
@@ -162,92 +61,83 @@ typedef struct _tool_fifo_api_t{
  */ 
 
 /*-------------------------------------
- *  tool_fifo_flush 
+ *  tool_fifo_t* _this_flush 
  */ 
-static inline void tool_fifo_flush(tool_fifo_t *fifo){  
-  fifo->head = fifo->tail = 0;
+static inline void tool_fifo_flush(tool_fifo_t* _this){  
+  _this->head = _this->tail = 0;
 }
 
-/*-------------------------------------
- *  tool_fifo_getSize 
- */ 
-static inline uint32_t tool_fifo_getSize(tool_fifo_t *fifo){
-  return fifo->itemSz;
-}
+
 
 /*-------------------------------------
- *  tool_fifo_getCount 
+ *  tool_fifo_t* _this_getSize 
  */ 
-static inline uint32_t tool_fifo_getCount(tool_fifo_t *fifo){
-  return fifo->count;
+static inline uint32_t tool_fifo_getSize(tool_fifo_t* _this){
+  return _this->count;
 }
 
-/*-------------------------------------
- *  tool_fifo_getFree 
- */ 
-static inline uint32_t tool_fifo_getFree(tool_fifo_t *fifo){
-  if(fifo->head>fifo->tail)
-    return (fifo->count-1) - fifo->head + fifo->tail;
-  
-  else if(fifo->head<fifo->tail)
-    return fifo->tail - fifo->head - 1;
-  
-  else
-    return (fifo->count-1);  
-}
+
 
 /*-------------------------------------
- *  tool_fifo_getUsed 
+ *  tool_fifo_t* _this_getCount 
  */ 
-static inline uint32_t tool_fifo_getUsed(tool_fifo_t *fifo){
-  return fifo->count - tool_fifo_getFree(fifo) - 1;
+static inline uint32_t tool_fifo_getCount(tool_fifo_t* _this){
+  return RB_VHEAD(_this) - RB_VTAIL(_this);
 }
 
-/*-------------------------------------
- *  tool_fifo_getStatus 
- */ 
-static inline uint8_t tool_fifo_getStatus(tool_fifo_t *fifo){
-  return fifo->config.status;
-}
+
 
 /*-------------------------------------
- *  tool_fifo_isFull 
+ *  tool_fifo_t* _this_getFree 
  */ 
-static inline bool tool_fifo_isFull(tool_fifo_t *fifo){
-  return (tool_fifo_getFree(fifo)==0)?true:false;
+static inline uint32_t tool_fifo_getFree(tool_fifo_t* _this){
+  return _this->count - tool_fifo_getCount(_this);
 }
 
+
+
 /*-------------------------------------
- *  tool_fifo_isEmpty
+ *  tool_fifo_t* _this_isFull 
  */ 
-static inline bool tool_fifo_isEmpty(tool_fifo_t *fifo){
-  return (tool_fifo_getFree(fifo))==(fifo->count-1)?true:false;
+static inline bool tool_fifo_isFull(tool_fifo_t* _this){
+  return (tool_fifo_getCount(_this) >= _this->count);
 }
+
+
+
+/*-------------------------------------
+ *  tool_fifo_t* _this_isEmpty
+ */ 
+static inline bool tool_fifo_isEmpty(tool_fifo_t* _this){
+  return RB_VHEAD(_this) == RB_VTAIL(_this);
+}
+
+
 
 /* *****************************************************************************************
  *    Link API
  */ 
 extern const tool_fifo_api_t tool_fifo_api;
 
+
+
 /* *****************************************************************************************
  *    Method list
  */ 
-bool tool_fifo_init(tool_fifo_t *fifo,  const tool_fifo_config_t *config);
-tool_fifo_t* tool_fifo_initAddr(const tool_fifo_config_t *config);
-tool_fifo_t* tool_fifo_reload(const tool_fifo_config_t *config);
-bool tool_fifo_insert(tool_fifo_t *fifo, const void *data);
-int tool_fifo_insertMulti(tool_fifo_t *fifo, const void *data, uint32_t count);
-bool tool_fifo_insertTail(tool_fifo_t *fifo, const void *data);
-bool tool_fifo_pop(tool_fifo_t *fifo, void *buffer);
-int tool_fifo_popMulti(tool_fifo_t *fifo, void *buffer, uint32_t count);
-bool tool_fifo_popHead(tool_fifo_t *fifo, void *buffer);
-void tool_fifo_setFunc(tool_fifo_t *fifo, uint8_t setting);
-void tool_fifo_unsetFunc(tool_fifo_t *fifo, uint8_t setting);
+bool tool_fifo_init(tool_fifo_t* _this, void *buffer, int itemSize, int count);
+bool tool_fifo_insert(tool_fifo_t* fifo, const void *data);
+int tool_fifo_insertMulti(tool_fifo_t* fifo, const void *data, uint32_t count);
+bool tool_fifo_insertTail(tool_fifo_t* fifo, const void *data);
+bool tool_fifo_pop(tool_fifo_t* fifo, void *buffer);
+int tool_fifo_popMulti(tool_fifo_t* fifo, void *buffer, uint32_t count);
+bool tool_fifo_popHead(tool_fifo_t* fifo, void *buffer);
+
+
 
 #ifdef __cplusplus
 }
-#endif
-#endif
+#endif //__cplusplus
+#endif //tool_fifo_H_
 /* *****************************************************************************************
  *  End of file
  */
--- source/tool_fifo.c
+++ source/tool_fifo.c
@@ -1,311 +1,202 @@
 /* *****************************************************************************************
  *      File Name    :tool_fifo.c
  *      Create Date  :2018-09-09
- *      Modufy Date  :2021-03-28
+ *      Modufy Date  :2021-04-20
  *      Information :
  */
 #include "tool_fifo.h"
 #include "string.h" 
-#include "ztool.h"
 
 /* *****************************************************************************************
  *  Macro Function 
  */
-#define TOOL_FIFO_RS(src, max) if(src<(max-1)) src++; else src=0;
-#define TOOL_FIFO_LS(src, max) if(src>(0)) src--; else src=(max-1);
+#define RB_INDH(rb)                ((rb)->head & ((rb)->count - 1))
+#define RB_INDT(rb)                ((rb)->tail & ((rb)->count - 1))
 
-#define TOOL_FIFO_RSMULT(src, max, count) if((max-src)>count) src+=count; else src=(count-(max-src))
-#define TOOL_FIFO_LSMULT(src, max, count) if(src<(count)) src=(src+max-count); else src=(src-count)
+#if !defined(MAX)
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#endif
+#if !defined(MIN)
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#endif
+
 /* *****************************************************************************************
  *  Private Function 
  */
- 
-/*-------------------------------------
- *  tool_fifo_memcpy 
- */ 
-static void* tool_fifo_memcpy(void *dst, const void *src, uint32_t len){
-  char *d = dst;
-  const char *s = src;
-  while (len--)
-    *d++ = *s++;
-  return dst;
-}
+
 /* *****************************************************************************************
  *  Public Function 
  */
-
-/*-------------------------------------
+  
+/*----------------------------------------
  *  tool_fifo_init
- */ 
-bool tool_fifo_init(tool_fifo_t *fifo,  const tool_fifo_config_t *config){
-  memset(fifo, 0x00, sizeof(tool_fifo_t));
-  fifo->data = config->buffer;
-  fifo->count = config->count;
-  fifo->itemSz = config->itemSize;
-  fifo->head = fifo->tail = 0;
-  
+ *----------------------------------------*/ 
+bool tool_fifo_init(tool_fifo_t* _this, void *buffer, int itemSize, int count){
+  _this->data = buffer;
+  _this->count = count;
+  _this->itemSz = itemSize;
+  _this->head = _this->tail = 0;
+
   return true;
 }
 
-/*-------------------------------------
- *  tool_fifo_initAddr
- */ 
-tool_fifo_t* tool_fifo_initAddr(const tool_fifo_config_t *config){
-  
-  tool_fifo_t* fifo = (tool_fifo_t*)config->address;
-  memset(fifo, 0x00, sizeof(tool_fifo_t));
-  
-  uint32_t temp = (uint32_t)config->address;
-  temp += sizeof(tool_fifo_t);
-  
-  fifo->data = (void*)temp;
 
-  fifo->count = ((config->bufferSize - sizeof(tool_fifo_t))/config->itemSize);
-  fifo->itemSz = config->itemSize;
-  fifo->head = fifo->tail = 0;
-    
-  return fifo;
-}
 
-/*-------------------------------------
- *  tool_fifo_reload
- */ 
-tool_fifo_t* tool_fifo_reload(const tool_fifo_config_t *config){
-  tool_fifo_t* fifo = (tool_fifo_t*)config->address;
-  
-  void* pData = (void*)((uint32_t)config->address + sizeof(tool_fifo_t));
-  int count = ((config->bufferSize - sizeof(tool_fifo_t))/config->itemSize);
-  
-  if(fifo->data != pData)
-    return (tool_fifo_t*)0x00000000;
-    
-  if(fifo->count != count)
-    return (tool_fifo_t*)0x00000000;
-    
-  if(fifo->itemSz != config->itemSize)
-    return (tool_fifo_t*)0x00000000;
-    
-  return fifo;
-}
-
-/*-------------------------------------
+/*----------------------------------------
  *  tool_fifo_insert
- */ 
-bool tool_fifo_insert(tool_fifo_t *fifo, const void *data){
-  if ((tool_fifo_isFull(fifo) & isGBitLow(fifo->config.func, fifo_func_override))) 
-    return false;  
-  
-  //SetMutexInsert
-  if(isGBitHigh(fifo->config.func, fifo_func_mutex) & isGBitHigh(fifo->config.status, fifo_status_isMutexHead))
-    return false;  
-  setGBitHigh(fifo->config.status, fifo_status_isMutexHead);
-  
-  uint8_t *ptr = fifo->data;
-  ptr += fifo->head * fifo->itemSz;
-  tool_fifo_memcpy(ptr, data, fifo->itemSz);
-  TOOL_FIFO_RS(fifo->head, fifo->count);    
+ *----------------------------------------*/ 
+bool tool_fifo_insert(tool_fifo_t* _this, const void* data){
+  uint8_t *ptr = _this->data;
 
-  if(tool_fifo_isFull(fifo)){
-    TOOL_FIFO_RS(fifo->tail, fifo->count);
-  }
-  
-  //UnsetMutexInsert
-  setGBitLow(fifo->config.status, fifo_status_isMutexHead);
+  /* We cannot insert when queue is full */
+  if (tool_fifo_isFull(_this))
+    return false;
+
+  ptr += RB_INDH(_this) * _this->itemSz;
+  memcpy(ptr, data, _this->itemSz);
+  _this->head++;
+
   return true;
 }
 
-/*-------------------------------------
+
+
+/*----------------------------------------
  *  tool_fifo_insertMulti
- */ 
-int tool_fifo_insertMulti(tool_fifo_t *fifo, const void *data, uint32_t count){
-  if ((tool_fifo_isFull(fifo) & isGBitLow(fifo->config.func, fifo_func_override))) 
-    return count;  
-  
-  //SetMutexInsert
-  if(isGBitHigh(fifo->config.func, fifo_func_mutex) & isGBitHigh(fifo->config.status, fifo_status_isMutexHead))
-    return count;
-  setGBitHigh(fifo->config.status, fifo_status_isMutexHead);  
-  
-  uint8_t *ptr;
-  uint32_t headToEnd = fifo->count - fifo->head;
-  uint32_t overflowCount = 0;
-  if(tool_fifo_getFree(fifo)<count)
-    overflowCount = count - tool_fifo_getFree(fifo);
-  
-  //start copy
-  ptr = ((uint8_t*)fifo->data) + (fifo->head * fifo->itemSz);
-  
-  if(isGBitLow(fifo->config.func, fifo_func_override)){
-    //func override is disable
-    if((count - overflowCount) > headToEnd){
-      //need segmentation
-      
-      tool_fifo_memcpy(ptr, data, (fifo->itemSz * headToEnd));
-      TOOL_FIFO_RSMULT(fifo->head, fifo->count, headToEnd);  
+ *----------------------------------------*/ 
+int tool_fifo_InsertMult(tool_fifo_t* _this, const void* data, int num){
+  uint8_t *ptr = _this->data;
+  int cnt1, cnt2;
 
-      ptr = ((uint8_t*)fifo->data) + (fifo->head * fifo->itemSz);
-      data = ((uint8_t*)data) + (headToEnd * fifo->itemSz);
-      
-      tool_fifo_memcpy(ptr, data, (fifo->itemSz * (count-(headToEnd + overflowCount))));
-      TOOL_FIFO_RSMULT(fifo->head, fifo->count, (count-(headToEnd + overflowCount)));
-    }
-    else{
-      tool_fifo_memcpy(ptr, data, (fifo->itemSz * (count - overflowCount)));
-      TOOL_FIFO_RSMULT(fifo->head, fifo->count, (count - overflowCount));      
-    }
-    
-    setGBitLow(fifo->config.status, fifo_status_isMutexHead);
-    return overflowCount;
-  }
-  else{
-    //func override is enable
-    if(count > headToEnd){
-      //need segmentation
-      tool_fifo_memcpy(ptr, data, (fifo->itemSz * headToEnd));
-      TOOL_FIFO_RSMULT(fifo->head, fifo->count, headToEnd);  
-
-      ptr = ((uint8_t*)fifo->data) + (fifo->head * fifo->itemSz);
-      data = ((uint8_t*)data) + (headToEnd * fifo->itemSz);      
-      
-      tool_fifo_memcpy(ptr, data, (fifo->itemSz * (count - headToEnd)));
-      TOOL_FIFO_RSMULT(fifo->head, fifo->count, (count - headToEnd));    
-      
-      TOOL_FIFO_RSMULT(fifo->tail, fifo->count, overflowCount);
-    }
-    else{
-      tool_fifo_memcpy(ptr, data, (fifo->itemSz * count));
-      TOOL_FIFO_RSMULT(fifo->head, fifo->count, count);      
-      
-      TOOL_FIFO_RSMULT(fifo->tail, fifo->count, overflowCount);
-    }
-    
-    setGBitLow(fifo->config.status, fifo_status_isMutexHead);
+  /* We cannot insert when queue is full */
+  if (tool_fifo_isFull(_this))
     return 0;
-  }
+
+  /* Calculate the segment lengths */
+  cnt1 = cnt2 = tool_fifo_getFree(_this);
+  if (RB_INDH(_this) + cnt1 >= _this->count)
+    cnt1 = _this->count - RB_INDH(_this);
+  cnt2 -= cnt1;
+
+  cnt1 = MIN(cnt1, num);
+  num -= cnt1;
+
+  cnt2 = MIN(cnt2, num);
+  num -= cnt2;
+
+  /* Write segment 1 */
+  ptr += RB_INDH(_this) * _this->itemSz;
+  memcpy(ptr, data, cnt1 * _this->itemSz);
+  _this->head += cnt1;
+
+  /* Write segment 2 */
+  ptr = (uint8_t *) _this->data + RB_INDH(_this) * _this->itemSz;
+  data = (const uint8_t *) data + cnt1 * _this->itemSz;
+  memcpy(ptr, data, cnt2 * _this->itemSz);
+  _this->head += cnt2;
+
+  return cnt1 + cnt2;
 }
 
-/*-------------------------------------
+
+
+/*----------------------------------------
  *  tool_fifo_insertTail
- */ 
-bool tool_fifo_insertTail(tool_fifo_t *fifo, const void *data){
-  if ((tool_fifo_isFull(fifo) & isGBitLow(fifo->config.func, fifo_func_override))) 
-    return false;  
-  
-  //SetMutexInsert
-  if(isGBitHigh(fifo->config.func, fifo_func_mutex) & isGBitHigh(fifo->config.status, fifo_status_isMutexTail))
-    return false;
-  setGBitHigh(fifo->config.status, fifo_status_isMutexTail);  
-  
-  uint8_t *ptr = fifo->data;
-  TOOL_FIFO_LS(fifo->tail, fifo->count);
-  ptr += fifo->tail * fifo->itemSz;
-  tool_fifo_memcpy(ptr, data, fifo->itemSz);
+ *----------------------------------------*/ 
+bool tool_fifo_insertTail(tool_fifo_t* _this, const void* data){
+  uint8_t *ptr = _this->data;
 
-  if(tool_fifo_isFull(fifo)){
-    TOOL_FIFO_LS(fifo->head, fifo->count);
-  }
-    
-  setGBitLow(fifo->config.status, fifo_status_isMutexTail);
+  /* We cannot insert when queue is full */
+  if (tool_fifo_isFull(_this))
+    return false;
+
+  ptr += (RB_INDT(_this)-1) * _this->itemSz;
+  memcpy(ptr, data, _this->itemSz);
+  _this->tail--;
+
   return true;
 }
 
-/*-------------------------------------
+
+
+/*----------------------------------------
  *  tool_fifo_pop
- */ 
-bool tool_fifo_pop(tool_fifo_t *fifo, void *buffer){
-  if (tool_fifo_isEmpty(fifo)) 
-    return false;  
-  //SetMutexInsert
-  if(isGBitHigh(fifo->config.func, fifo_func_mutex) & isGBitHigh(fifo->config.status, fifo_status_isMutexTail))
-    return false;
-  setGBitHigh(fifo->config.status, fifo_status_isMutexTail);    
-  
-  uint8_t *ptr = fifo->data;
-  ptr += fifo->tail * fifo->itemSz;
-  tool_fifo_memcpy(buffer, ptr, fifo->itemSz);
-  TOOL_FIFO_RS(fifo->tail, fifo->count);
+ *----------------------------------------*/ 
+bool tool_fifo_Pop(tool_fifo_t* _this, void *data){
+  uint8_t *ptr = _this->data;
 
-  setGBitLow(fifo->config.status, fifo_status_isMutexTail);  
+  /* We cannot pop when queue is empty */
+  if (tool_fifo_isEmpty(_this))
+    return false;
+
+  ptr += RB_INDT(_this) * _this->itemSz;
+  memcpy(data, ptr, _this->itemSz);
+  _this->tail++;
+
   return true;
 }
 
-/*-------------------------------------
+
+
+/*----------------------------------------
  *  tool_fifo_popMulti
- */ 
-int tool_fifo_popMulti(tool_fifo_t *fifo, void *buffer, uint32_t count){
-  if (tool_fifo_isEmpty(fifo)) 
-    return 0;  
-  //SetMutexInsert
-  if(isGBitHigh(fifo->config.func, fifo_func_mutex) & isGBitHigh(fifo->config.status, fifo_status_isMutexTail))
-    return 0;
-  setGBitHigh(fifo->config.status, fifo_status_isMutexTail);
-  
-  uint8_t *ptr;
-  uint32_t tailToEnd = fifo->count - fifo->tail;
-  uint32_t rCount;
-  
-  if(tool_fifo_getUsed(fifo)>count)
-    rCount = count;
-  else
-    rCount = tool_fifo_getUsed(fifo);
-  
-  ptr = ((uint8_t*)fifo->data) + (fifo->tail * fifo->itemSz);
-  
-  
-  if(rCount > tailToEnd){
-    tool_fifo_memcpy(buffer, ptr, (tailToEnd * fifo->itemSz));
-    TOOL_FIFO_RSMULT(fifo->tail, fifo->count, tailToEnd);  
-    
-    ptr = ((uint8_t*)fifo->data) + (fifo->tail * fifo->itemSz);
-    buffer = ((uint8_t*)buffer) + (tailToEnd * fifo->itemSz);      
-    
-    tool_fifo_memcpy(buffer, ptr, ((rCount - tailToEnd) * fifo->itemSz));
-    TOOL_FIFO_RSMULT(fifo->tail, fifo->count, (rCount - tailToEnd));    
-  
-  }
-  else{
-    
-    tool_fifo_memcpy(buffer, ptr, (rCount * fifo->itemSz));
-    TOOL_FIFO_RSMULT(fifo->tail, fifo->count, rCount);
-  }
+ *----------------------------------------*/ 
+int tool_fifo_PopMult(tool_fifo_t* _this, void *data, int num)
+{
+  uint8_t *ptr = _this->data;
+  int cnt1, cnt2;
 
-  setGBitLow(fifo->config.status, fifo_status_isMutexTail);
-  return rCount;
+  /* We cannot insert when queue is empty */
+  if (tool_fifo_isEmpty(_this))
+    return 0;
+
+  /* Calculate the segment lengths */
+  cnt1 = cnt2 = tool_fifo_getCount(_this);
+  if (RB_INDT(_this) + cnt1 >= _this->count)
+    cnt1 = _this->count - RB_INDT(_this);
+  cnt2 -= cnt1;
+
+  cnt1 = MIN(cnt1, num);
+  num -= cnt1;
+
+  cnt2 = MIN(cnt2, num);
+  num -= cnt2;
+
+  /* Write segment 1 */
+  ptr += RB_INDT(_this) * _this->itemSz;
+  memcpy(data, ptr, cnt1 * _this->itemSz);
+  _this->tail += cnt1;
+
+  /* Write segment 2 */
+  ptr = (uint8_t *) _this->data + RB_INDT(_this) * _this->itemSz;
+  data = (uint8_t *) data + cnt1 * _this->itemSz;
+  memcpy(data, ptr, cnt2 * _this->itemSz);
+  _this->tail += cnt2;
+
+  return cnt1 + cnt2;
 }
 
-/*-------------------------------------
- *  tool_fifo_popHead
- */ 
-bool tool_fifo_popHead(tool_fifo_t *fifo, void *buffer){
-  if (tool_fifo_isEmpty(fifo)) 
-    return false;  
-  //SetMutexInsert
-  if(isGBitHigh(fifo->config.func, fifo_func_mutex) & isGBitHigh(fifo->config.status, fifo_status_isMutexHead))
-    return false;
-  setGBitHigh(fifo->config.status, fifo_status_isMutexHead);  
-  
-  uint8_t *ptr = fifo->data;
-  
-  TOOL_FIFO_LS(fifo->head, fifo->count);
-  ptr += fifo->head * fifo->itemSz;
-  tool_fifo_memcpy(buffer, ptr, fifo->itemSz);
 
-  setGBitLow(fifo->config.status, fifo_status_isMutexHead);    
+
+/*----------------------------------------
+ *  tool_fifo_popHead
+ *----------------------------------------*/
+
+bool tool_fifo_popHead(tool_fifo_t* _this, void *data){
+  uint8_t *ptr = _this->data;
+
+  /* We cannot pop when queue is empty */
+  if (tool_fifo_isEmpty(_this))
+    return false;
+
+  ptr += (RB_INDH(_this)-1) * _this->itemSz;
+  memcpy(data, ptr, _this->itemSz);
+  _this->head--;
+
   return true;
 }
-/*-------------------------------------
- *  tool_fifo_setFunc
- */ 
-void tool_fifo_setFunc(tool_fifo_t *fifo, uint8_t setting){
-  setGBitHigh(fifo->config.func, setting);
-}
 
-/*-------------------------------------
- *  tool_fifo_unsetFunc
- */ 
-void tool_fifo_unsetFunc(tool_fifo_t *fifo, uint8_t setting){
-  setGBitLow(fifo->config.func, setting);
-}
+
 
 /* *****************************************************************************************
  *  API Link
@@ -315,21 +206,15 @@ const tool_fifo_api_t tool_fifo_api = {
   .getSize       = tool_fifo_getSize,
   .getCount      = tool_fifo_getCount,
   .getFree       = tool_fifo_getFree,
-  .getUsed       = tool_fifo_getUsed,
-  .getStatus     = tool_fifo_getStatus,
   .isFull        = tool_fifo_isFull,
   .isEmpty       = tool_fifo_isEmpty,
   .init          = tool_fifo_init,
-  .initAddr      = tool_fifo_initAddr,
-  .reload        = tool_fifo_reload,
   .insert        = tool_fifo_insert,
   .insertMulti   = tool_fifo_insertMulti,
   .insertTail    = tool_fifo_insertTail,
   .pop           = tool_fifo_pop,
   .popMulti      = tool_fifo_popMulti,
   .popHead       = tool_fifo_popHead,
-  .setFunc       = tool_fifo_setFunc,
-  .unsetFunc     = tool_fifo_unsetFunc,
 };
 
 /* *****************************************************************************************
